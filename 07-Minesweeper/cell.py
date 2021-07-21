3ec6353e6d9b2266b4f26904267ca4ae5920c657
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2021-07-21 07:01:07 +0530

    finish minesweeper

--- 07-Minesweeper/cell.py
+++ 07-Minesweeper/cell.py
@@ -1,10 +1,112 @@
 import pyglet
-from pyglet import shapes
 
-class Cell(shapes.Rectangle):
-    def __init__(self):
-        super(Cell, self).__init__(x, y, w, h)
-        self.x = x
-        self.y = y
-        self.width = w
-        self.height = h
+mine = pyglet.image.load('./assets/mine.png')
+flag = pyglet.image.load('./assets/flag.png')
+
+
+class Cell(pyglet.shapes.Rectangle):
+    def __init__(self, x, y, width, height):
+        super(Cell, self).__init__(x, y, width, height)
+
+        self.isActive = False
+        self.isFlagged = False
+        self.isMine = False
+
+        self.cell_size = 50
+        self.secondaryColor = (100, 100, 100)
+        self.count = 0 # neighbor count
+
+        # sprites
+        self.flagSprite = pyglet.sprite.Sprite(img=flag)
+        self.mineSprite = pyglet.sprite.Sprite(img=mine)
+        self.flagSprite.scale = 1/(width/2)
+        self.flagSprite.x = self.x + width/3
+        self.flagSprite.y = self.y + width/3
+        self.mineSprite.scale = 1/(width/3)
+        self.mineSprite.x = self.x + self.mineSprite.width/3
+        self.mineSprite.y = self.y + self.mineSprite.width/3
+
+
+
+    def update(self, m):
+        if self.isActive == True:
+            self.color = self.secondaryColor
+
+    def floodFill(x, y ,grid):
+        if Cell.isFillable(x, y, grid):
+            grid[x][y].isActive = True
+            if grid[x][y].count == 0 :
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        _x = x + i
+                        _y = y + j
+                        if _x != x or _y != y:
+                            Cell.floodFill(_x, _y, grid)
+
+        return
+
+    def isFillable(x, y, grid):
+        if x >= 0 and y >= 0 and x < len(grid) and y < len(grid[0]):
+            if grid[x][y].isActive != True and grid[x][y].isMine == False:
+                print("here2")
+                return True
+            else:
+                return False
+
+        return False
+    def activate(self, x, y, grid, Event="LEFT"):
+        if Event == "RIGHT":
+            self.isFlagged = not self.isFlagged
+        else:
+            if self.count == 0:
+                print("here1")
+                Cell.floodFill(x, y, grid)
+            self.isActive = True
+
+
+
+    def countMines(self, grid):
+        counter = 0
+        if self.isMine == True:
+            self.count = -1
+            return
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                if self.x//self.cell_size + x < len(grid) and self.y//self.cell_size + y < len(grid[0]):
+                    neighbor = grid[self.x//self.cell_size + x ][ self.y//self.cell_size + y]
+                    if neighbor.isMine == True:
+                        counter += 1
+        self.count =  counter
+        color = (0, 0, 255, 255)
+        if self.count <= 1:
+            color = (0, 0, 255, 255)
+        elif self.count == 2:
+            color = (0, 255, 0, 255)
+        elif self.count == 3:
+            color = (255, 0, 0, 255)
+        else:
+            color = (255, 50, 255, 255)
+
+        if self.isMine == False:
+            self.text = pyglet.text.Label(
+            str(self.count),
+            x= self.x + self.cell_size//2,
+            y = self.y + self.cell_size//2,
+            anchor_x='center',
+            anchor_y='center',
+            font_size=30,
+            font_name="Verdana",
+            bold=True)
+            self.text.color = color
+
+
+
+    def show(self):
+        self.draw()
+        if self.isActive == True:
+            if self.isMine == True:
+                self.mineSprite.draw()
+            elif self.count > 0:
+                self.text.draw()
+        elif self.isFlagged == True:
+            self.flagSprite.draw()
--- 07-Minesweeper/main.py
+++ 07-Minesweeper/main.py
@@ -1,12 +1,85 @@
 import pyglet
 from cell import Cell
+from pyglet import clock
+from pyglet.window import mouse, key
+import random
 
 width, height = 500, 500
+white=(255, 255, 255)
 window = pyglet.window.Window(width, height)
-cell = Cell(width//2, height//2, 100, 100)
-cell.color = (255, 0, 0)
+
+cell_size = 50
+rows = height//cell_size
+columns = width//cell_size
+
+minefield = 20
+options = []
+
+class Mouse:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.left = False
+        self.right = False
+    def reset(self):
+        m.left = False
+        m.right = False
+
+m = Mouse()
+
+# generate Grid
+Grid = [[None for _ in range(columns)] for i in range(rows)]
+for x in range(rows):
+    for y in range(columns):
+        cell = Cell(x * cell_size, y*cell_size, cell_size-1, cell_size-1)
+        #cell.color = white
+        Grid[x][y] = cell
+        options.append((x, y))
+
+# set the mine cells
+for i in range(minefield):
+    choice = random.choice(options)
+    options.remove(choice)
+    x, y = choice
+    Grid[x][y].isMine = True
+# get the number of mines
+for x in range(rows):
+    for y in range(columns):
+        Grid[x][y].countMines(Grid)
+
+def Update(_dt):
+    index_x, index_y = m.x//cell_size, m.y//cell_size
+    if Grid[index_x][index_y] and m.left == True:
+        Grid[index_x][index_y].activate(index_x, index_y, Grid)
+    elif Grid[index_x][index_y] and m.right == True:
+        Grid[index_x][index_y].activate(index_x, index_y, Grid,"RIGHT")
+
+    for x in range(rows):
+        for y in range(columns):
+            Grid[x][y].update(m)
+
+    m.reset()
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    m.x, m.y = x, y
+    if button == mouse.LEFT:
+        m.left = True
+
+    if button == mouse.RIGHT:
+        m.right = True
+
+
+
 @window.event
 def on_draw():
+    window.set_caption (f'FPS : {pyglet.clock.get_fps()}')
     window.clear()
-    cell.draw()
+    for x in range(rows):
+        for y in range(columns):
+            Grid[x][y].show()
+
+clock.schedule_interval(Update, 1/30)
+
+
 pyglet.app.run()
